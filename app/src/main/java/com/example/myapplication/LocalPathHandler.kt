688bdeb3277f8c2578282e65bab15653174a899e
smikhalevski <smikhalevski@gmail.com>
2022-10-24 22:00:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/LocalPathHandler.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.webkit.WebResourceResponse
+import androidx.annotation.WorkerThread
+import androidx.webkit.WebViewAssetLoader
+import java.io.File
+import java.io.FileInputStream
+import java.net.URLConnection
+
+class LocalPathHandler(private val baseDirectory: File) : WebViewAssetLoader.PathHandler {
+
+    private val basePath = baseDirectory.absolutePath
+
+    init {
+        require(baseDirectory.isDirectory && baseDirectory.canRead()) {
+            "Cannot serve contents of a directory ${baseDirectory.absolutePath}"
+        }
+    }
+
+    @WorkerThread
+    override fun handle(path: String): WebResourceResponse {
+        var file = File(baseDirectory, path)
+
+        if (file.absolutePath.startsWith(basePath)) {
+            if (file.isDirectory) {
+                file = File(file, "index.html")
+            }
+            if (file.isFile && file.canRead()) {
+                return WebResourceResponse(URLConnection.guessContentTypeFromName(path), null, FileInputStream(file))
+            }
+        }
+
+        return WebResourceResponse(null, null, null)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/LocalWebViewClient.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.net.Uri
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import androidx.annotation.RequiresApi
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
+
+class LocalWebViewClient(private val host: String, private val assetLoader: WebViewAssetLoader) :
+    WebViewClientCompat() {
+    @RequiresApi(21)
+    override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
+        return if (request.url.host == host) assetLoader.shouldInterceptRequest(request.url) else null
+    }
+
+    // Support API < 21
+    override fun shouldInterceptRequest(view: WebView, url: String): WebResourceResponse? {
+        val uri = Uri.parse(url)
+
+        return if (uri.host == host) assetLoader.shouldInterceptRequest(uri) else null
+    }
+}
