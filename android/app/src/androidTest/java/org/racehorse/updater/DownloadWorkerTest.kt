5f71d9816124f81cdbcf12daae86fb63ea7981b7
smikhalevski <smikhalevski@gmail.com>
2023-02-01 16:52:10 +0300

    Added DownloadWorker tests

--- android/app/src/androidTest/java/org/racehorse/updater/DownloadWorkerTest.kt
+++ android/app/src/androidTest/java/org/racehorse/updater/DownloadWorkerTest.kt
@@ -9,23 +9,93 @@ import com.squareup.okhttp.mockwebserver.MockResponse
 import com.squareup.okhttp.mockwebserver.MockWebServer
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.File
+import java.net.HttpURLConnection
+import kotlin.concurrent.thread
 
 @RunWith(AndroidJUnit4::class)
 class DownloadWorkerTest {
+    private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    private val outputFile = File(appContext.filesDir, "output.txt")
+    private val partialFile = File(outputFile.absolutePath + DownloadWorker.PARTIAL_SUFFIX)
+    private val etagFile = File(outputFile.absolutePath + DownloadWorker.ETAG_SUFFIX)
+
+    @Before
+    fun before() {
+        DownloadWorker.deleteDownload(outputFile.absolutePath)
+    }
 
     @Test
-    fun testUninterruptedDownloadSucceeds() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    fun testDownloadsFile() {
+        val server = MockWebServer()
 
-        val responseBody = "aaa_aaa_aaa_aaa"
-        val outputFile = File(appContext.filesDir, "ccc.txt")
+        server.enqueue(MockResponse().setBody("xxx"))
+
+        server.play()
+
+        val worker = TestListenableWorkerBuilder<DownloadWorker>(appContext)
+            .setInputData(
+                workDataOf(
+                    DownloadWorker.URL to server.getUrl("/").toString(),
+                    DownloadWorker.PATHNAME to outputFile.absolutePath,
+                )
+            )
+            .build()
+
+        val result = runBlocking {
+            worker.doWork()
+        }
+
+        Assert.assertTrue(result is ListenableWorker.Result.Success)
+        Assert.assertTrue(outputFile.exists())
+        Assert.assertEquals("xxx", outputFile.readText())
+    }
+
+    @Test
+    fun testOverwritesPreviouslyDownloadedFile() {
+        outputFile.writeText("xxx")
+
+        Assert.assertEquals("xxx", outputFile.readText())
 
         val server = MockWebServer()
 
-        server.enqueue(MockResponse().setBody(responseBody).setHeader("ETag", "bbb"))
+        server.enqueue(MockResponse().setBody("yyy"))
+
+        server.play()
+
+        val worker = TestListenableWorkerBuilder<DownloadWorker>(appContext)
+            .setInputData(
+                workDataOf(
+                    DownloadWorker.URL to server.getUrl("/").toString(),
+                    DownloadWorker.PATHNAME to outputFile.absolutePath,
+                )
+            )
+            .build()
+
+        val result = runBlocking {
+            worker.doWork()
+        }
+
+        Assert.assertTrue(result is ListenableWorker.Result.Success)
+        Assert.assertEquals("yyy", outputFile.readText())
+    }
+
+    @Test
+    fun testResumesPartialDownload() {
+        partialFile.writeText("111_22")
+        etagFile.writeText("bbb")
+
+        val server = MockWebServer()
+
+        server.enqueue(
+            MockResponse()
+                .setResponseCode(HttpURLConnection.HTTP_PARTIAL)
+                .setHeader("ETag", "bbb")
+                .setBody("2_333")
+        )
         server.play()
 
         val worker = TestListenableWorkerBuilder<DownloadWorker>(appContext)
@@ -38,14 +108,61 @@ class DownloadWorkerTest {
             )
             .build()
 
-        runBlocking {
-            val result = worker.doWork()
-
-            Assert.assertTrue(result is ListenableWorker.Result.Success)
-            Assert.assertEquals(15, result.outputData.getLong(DownloadWorker.CONTENT_LENGTH, 0))
-            Assert.assertEquals(15, result.outputData.getLong(DownloadWorker.READ_LENGTH, 0))
-            Assert.assertEquals(true, outputFile.exists())
-            Assert.assertEquals(responseBody, outputFile.readText())
+        val result = runBlocking {
+            worker.doWork()
         }
+
+        val request = server.takeRequest()
+
+        Assert.assertEquals("GET", request.method)
+        Assert.assertEquals("bytes=6-", request.getHeader("Range"))
+        Assert.assertEquals("bbb", request.getHeader("If-Range"))
+
+        Assert.assertTrue(result is ListenableWorker.Result.Success)
+        Assert.assertTrue(outputFile.exists())
+        Assert.assertEquals("111_222_333", outputFile.readText())
+    }
+
+    @Test
+    fun testPreservesPartialDownloadAfterStop() {
+        val server = MockWebServer()
+
+        server.enqueue(
+            MockResponse()
+                .setResponseCode(HttpURLConnection.HTTP_OK)
+                .setHeader("ETag", "bbb")
+                .setBody("111_222_333")
+                .setBytesPerSecond(2)
+        )
+
+        server.play()
+
+        val worker = TestListenableWorkerBuilder<DownloadWorker>(appContext)
+            .setInputData(
+                workDataOf(
+                    DownloadWorker.URL to server.getUrl("/").toString(),
+                    DownloadWorker.PATHNAME to outputFile.absolutePath,
+                    DownloadWorker.BUFFER_SIZE to 2
+                )
+            )
+            .build()
+
+        runBlocking {
+            thread {
+                Thread.sleep(1500L)
+                worker.stop()
+            }
+            worker.doWork()
+        }
+
+        val request = server.takeRequest()
+
+        Assert.assertEquals("GET", request.method)
+        Assert.assertNull(request.getHeader("Range"))
+        Assert.assertNull(request.getHeader("If-Range"))
+
+        Assert.assertFalse(outputFile.exists())
+        Assert.assertEquals("111_22", partialFile.readText())
+        Assert.assertEquals("bbb", etagFile.readText())
     }
 }
--- android/app/src/main/java/org/racehorse/updater/DownloadWorker.kt
+++ android/app/src/main/java/org/racehorse/updater/DownloadWorker.kt
@@ -11,95 +11,92 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 /**
- * Downloads URL to a file, can continue previously started download if server responded with ETag header.
+ * Downloads URL response body to a local file at given pathname, can continue previously started download if server
+ * responded with ETag header.
  */
 open class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
 
     companion object {
-        /**
-         * The bundle archive URL.
-         */
         const val URL = "URL"
-
-        /**
-         * The pathname where the bundle archive should be saved.
-         */
         const val PATHNAME = "PATHNAME"
-
-        /**
-         * The number of bytes to read in one chunk.
-         */
         const val BUFFER_SIZE = "BUFFER_SIZE"
-
-        /**
-         * The total number of bytes in the downloaded file.
-         */
         const val CONTENT_LENGTH = "CONTENT_LENGTH"
-
-        /**
-         * The total number of bytes read.
-         */
         const val READ_LENGTH = "READ_LENGTH"
+
+        const val PARTIAL_SUFFIX = ".partial"
+        const val ETAG_SUFFIX = ".etag"
+
+        fun deleteDownload(pathname: String) {
+            File(pathname).delete()
+            File(pathname + PARTIAL_SUFFIX).delete()
+            File(pathname + ETAG_SUFFIX).delete()
+        }
     }
 
     open fun openConnection(url: String): HttpURLConnection {
         return URL(url).openConnection() as HttpURLConnection
     }
 
-    override suspend fun doWork(): Result {
+    override suspend fun doWork() = try {
+        downloadFile()
+    } catch (e: IOException) {
+        Result.retry()
+    }
+
+    private suspend fun downloadFile(): Result {
         val url = requireNotNull(inputData.getString(URL))
         val pathname = requireNotNull(inputData.getString(PATHNAME))
         val bufferSize = inputData.getInt(BUFFER_SIZE, 8192)
 
-        val tempFile = File("$pathname.temp")
-        val etagFile = File("$pathname.etag")
+        val partialFile = File(pathname + PARTIAL_SUFFIX)
+        val etagFile = File(pathname + ETAG_SUFFIX)
 
-        var readLength = 0L
+        var readLength = 0
         val connection = openConnection(url)
 
-        if (tempFile.exists() && etagFile.exists()) {
-            readLength = tempFile.length()
+        if (partialFile.exists() && etagFile.exists()) {
+            readLength = partialFile.length().toInt()
             connection.setRequestProperty("Range", "bytes=$readLength-")
             connection.setRequestProperty("If-Range", etagFile.readText())
         }
 
-        return try {
-            connection.inputStream.use { inputStream ->
-                val etag = connection.getHeaderField("ETag")
-                val partial = connection.responseCode == HttpURLConnection.HTTP_PARTIAL
-
-                if (!partial) {
-                    readLength = 0L
-                }
-                if (etag != null) {
-                    etagFile.writeText(etag)
-                }
-
-                FileOutputStream(tempFile, partial).use { outputStream ->
-                    val contentLength = connection.contentLength
-                    val buffer = ByteArray(bufferSize)
-
-                    while (!this@DownloadWorker.isStopped) {
-                        setProgress(workDataOf(CONTENT_LENGTH to contentLength, READ_LENGTH to readLength))
-
-                        val byteCount = inputStream.read(buffer)
-                        if (byteCount == -1) {
-                            break
-                        }
-
-                        readLength += byteCount
-                        outputStream.write(buffer, 0, byteCount)
-                    }
-                }
+        connection.inputStream.use { inputStream ->
+            if (connection.responseCode != HttpURLConnection.HTTP_PARTIAL && connection.responseCode != HttpURLConnection.HTTP_OK) {
+                return Result.retry()
+            }
+            if (connection.responseCode != HttpURLConnection.HTTP_PARTIAL) {
+                readLength = 0
             }
 
-            tempFile.renameTo(File(pathname))
-            etagFile.delete()
+            val etag = connection.getHeaderField("ETag")
+            if (etag != null) {
+                etagFile.writeText(etag)
+            }
 
-            return Result.success(workDataOf(CONTENT_LENGTH to readLength, READ_LENGTH to readLength))
-        } catch (_: IOException) {
-            Result.retry()
+            FileOutputStream(partialFile, readLength != 0).use { outputStream ->
+                val contentLength = connection.contentLength
+                val buffer = ByteArray(bufferSize)
+
+                while (!isStopped) {
+                    setProgress(workDataOf(CONTENT_LENGTH to contentLength, READ_LENGTH to readLength))
+
+                    val byteCount = inputStream.read(buffer)
+                    if (byteCount == -1) {
+                        break
+                    }
+
+                    readLength += byteCount
+                    outputStream.write(buffer, 0, byteCount)
+                }
+            }
         }
+
+        if (!isStopped && partialFile.renameTo(File(pathname))) {
+            etagFile.delete()
+            return Result.success()
+        }
+
+        return Result.failure()
     }
 }
