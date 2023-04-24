69db68bbef8915895ae9125dc85c7f440c217c68
smikhalevski <smikhalevski@gmail.com>
2023-04-24 19:33:22 +0300

    web-racehorse-0.0.4 web-react-0.0.4 android-racehorse-0.0.4

--- android/racehorse/build.gradle.kts
+++ android/racehorse/build.gradle.kts
@@ -53,7 +53,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "org.racehorse"
             artifactId = "racehorse"
-            version = "0.0.3"
+            version = "0.0.4"
 
             afterEvaluate {
                 from(components["release"])
