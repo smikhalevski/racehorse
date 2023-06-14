0325b008d06ddbdb5a86f00a608003bd0816dbd2
smikhalevski <smikhalevski@gmail.com>
2023-06-14 11:47:49 +0300

    CommonJS Rollup config

--- web/example/rollup.config.js
+++ web/example/rollup.config.js
@@ -1,13 +1,13 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import replace from '@rollup/plugin-replace';
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
-import zip from 'rollup-plugin-zip';
-import html from '@rollup/plugin-html';
+const nodeResolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const replace = require('@rollup/plugin-replace');
+const serve = require('rollup-plugin-serve');
+const livereload = require('rollup-plugin-livereload');
+const zip = require('rollup-plugin-zip');
+const html = require('@rollup/plugin-html');
 
-export default {
+module.exports = {
   input: './src/index.tsx',
   output: { dir: './dist', format: 'iife' },
   plugins: [
