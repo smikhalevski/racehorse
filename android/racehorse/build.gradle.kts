a5b85cdee2e731bfdfc0b382711a70d9b157d272
smikhalevski <smikhalevski@gmail.com>
2023-07-21 17:16:43 +0300

    android-racehorse-0.0.10

--- android/racehorse/build.gradle.kts
+++ android/racehorse/build.gradle.kts
@@ -53,7 +53,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "org.racehorse"
             artifactId = "racehorse"
-            version = "0.0.9"
+            version = "0.0.10"
 
             afterEvaluate {
                 from(components["release"])
