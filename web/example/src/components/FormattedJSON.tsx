23ee8572a2f5ada43ac4fcb3bdfe4eca31c2d080
smikhalevski <smikhalevski@gmail.com>
2023-09-21 23:28:28 +0300

    Self-review fixes

--- web/example/src/components/FormattedJSON.tsx
+++ web/example/src/components/FormattedJSON.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export function FormattedJSON({ value }: { value: any }) {
-  return (
-    <pre style={{ whiteSpace: 'pre-wrap' }}>{value === undefined ? 'undefined' : JSON.stringify(value, null, 2)}</pre>
-  );
+  return <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(value, null, 2)}</pre>;
 }
