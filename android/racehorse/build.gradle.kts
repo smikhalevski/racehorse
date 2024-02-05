a4c07aa903993b16ac6e0c17d4e97cc58a9cc8dc
smikhalevski <smikhalevski@gmail.com>
2024-02-05 15:48:04 +0300

    web-racehorse-1.6.1 android-racehorse-1.6.1

--- android/racehorse/build.gradle.kts
+++ android/racehorse/build.gradle.kts
@@ -52,7 +52,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "org.racehorse"
             artifactId = "racehorse"
-            version = "1.6.0"
+            version = "1.6.1"
 
             afterEvaluate {
                 from(components["release"])
