eb43e99366884e9365bf1c03df21a9d7a6e228d3
smikhalevski <smikhalevski@gmail.com>
2024-02-26 18:47:34 +0300

    Added AbortController polyfill to example app

--- web/example/src/index.tsx
+++ web/example/src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { eventBridge } from 'racehorse';
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 
 eventBridge.connect().then(() => {
   createRoot(document.body.appendChild(document.createElement('div'))).render(<App />);
